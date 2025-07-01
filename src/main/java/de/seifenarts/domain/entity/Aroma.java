39c2b511fec944bc7c0458686fc1c45228ed6851
Artem Davidovich <chumazek@gmail.com>
2025-07-01 19:21:42 +0200

    Aroma, Size

---
+++ src/main/java/de/seifenarts/domain/entity/Aroma.java
@@ -0,0 +1,25 @@
+package de.seifenarts.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Table(name = "aroma")
+public class Aroma {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Setter(AccessLevel.NONE)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    @NotBlank(message = "Title can't be blank.")
+    private String title;
+
+}
---
+++ src/main/java/de/seifenarts/domain/entity/Size.java
@@ -0,0 +1,5 @@
+package de.seifenarts.domain.entity;
+
+public enum Size {
+    S, M, L, XL
+}
