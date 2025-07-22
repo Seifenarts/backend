e073297c3f2ffecf0a73e110787281d81e64db05
Artem Davidovich <chumazek@gmail.com>
2025-07-22 20:24:54 +0200

    Repository level

---
+++ src/main/java/de/seifenarts/domain/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package de.seifenarts.domain.repository;
+
+import de.seifenarts.domain.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
---
+++ src/main/java/de/seifenarts/domain/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package de.seifenarts.domain.repository;
+
+import de.seifenarts.domain.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
